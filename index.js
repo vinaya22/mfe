769c6e3cc5bd3087210271929ecf50d455207c55
vinay <vinaya22@gmail.com>
2022-09-03 18:49:56 +0530

    git flow

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const path = require('path');
 
 const product = express();
 product.use(express.static(path.join(__dirname, 'products/dist')));
+product.use(express.static(path.join(__dirname, 'wishlist/dist')));
+
+product.get('/wishlist', (req, res) => {
+  res.sendFile(path.join(__dirname, 'wishlist/dist'));
+});
+product.get('/products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'products/dist'));
+});
 product.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'products/dist'));
   });
 const port1 = process.env.PORT1 || process.env.PORT || 3001;
 product.listen(port1, () => {
     console.log('products app started at port ', port1)
-});
-
-
-const wishList = express();
-wishList.use(express.static(path.join(__dirname, 'wishlist/dist')));
-wishList.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'wishlist/dist'));
-  });
-const port2 = process.env.PORT2 || 3002;
-wishList.listen(port2, () => {
-    console.log('wishlist app started at port ', port2)
 });
